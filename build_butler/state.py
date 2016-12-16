f857fb309ed53f84917124d9df7d76bc7891a528
Chris Shumaker <chris.shumaker@gmail.com>
2016-12-16 16:48:06 -0500

    Cleaning up State class

--- build_butler/state.py
+++ build_butler/state.py
@@ -1,23 +1,8 @@
 class State:
 
-	def action(self):
-		print("undefined actioned")
-		self.next = None
-
 	def start(self):
 		self.action()
 		if(self.next != None):
 			self.next.start()
 		else:
 			return
-
-def action(self):
-	print("undefined actioned")
-	self.next = None
-
-def start(self):
-	self.action()
-	if(self.next != None):
-		self.next.start()
-	else:
-		return
