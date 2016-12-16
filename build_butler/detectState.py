385f17b032df991ab2a67a5adea801fddaf07da1
Chris Shumaker <chris.shumaker@gmail.com>
2016-12-16 17:29:15 -0500

    Porting face detection code into FSM

--- build_butler/detectState.py
+++ build_butler/detectState.py
@@ -1,14 +1,38 @@
 from build_butler import state, recognizeState, wanderState
 from build_butler.textToSpeech import tts
 import time
+import cv2
+import sys
 
 class DetectState(state.State):
 
+	RATIO = 92/112
+	CLASSIFIER_FILE = "sample_code/visual_recognition/find_my_face_live/haarcascade_frontalface_default.xml"
+
 	def action(self):
 		tts.say("Scanning for humans")
-		time.sleep(1)
-		detected = True
-		if(detected):
-			self.next = recognizeState.RecognizeState(data="Image")
+
+		cascPath = self.CLASSIFIER_FILE
+		faceCascade = cv2.CascadeClassifier(cascPath)
+		video_capture = cv2.VideoCapture(0)
+
+		# Throw away blank first frame
+		video_capture.read()
+		ret, frame = video_capture.read()
+
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+		faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30, 30),
+			flags=cv2.CASCADE_SCALE_IMAGE
+		)
+
+		video_capture.release()
+
+		if(faces.size > 0):
+			self.next = recognizeState.RecognizeState(data=[frame, faces])
 		else:
 			self.next = wanderState.WanderState()
